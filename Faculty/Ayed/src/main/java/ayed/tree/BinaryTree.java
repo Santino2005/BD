2a768499562a9df32dd2f9948482a3918d3377e3
Santino Colombo <scolombo2@mail.austral.edu.ar>
2024-03-21 18:09:41 -0300

    Java en git

---
+++ Faculty/Ayed/src/main/java/ayed/tree/BinaryTree.java
@@ -0,0 +1,15 @@
+class BinaryTree{
+	public static main(String[] args){
+		public void FizzBuzz(int Number);
+		}
+	
+	public void FizzBuzz(Number){
+
+		if(Number % 2 == 0){
+		System.out.println("Tu numero es par");
+		} else if(Number % 2 != 0){
+			System.out.println("No es par");
+			}
+		}
+	}
+			
